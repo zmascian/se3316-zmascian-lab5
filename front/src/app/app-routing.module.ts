f0c67f6c373d908c91c83ac1f123815fcd71c705
zmascian <zmascian@uwo.ca>
2019-12-02 13:06:35 -0500

    login and home component

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Add this
+import { LoginComponent } from './login/login.component'; // Add this
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },              // Add this
+  { path: 'login', component: LoginComponent }           // Add this
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
